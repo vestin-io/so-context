a2b1dd4cd3557563724390343b052d5458b17d3d
Jay Ji <jiqiang90@hotmail.com>
2026-06-07 15:03:45 +1200

    Keep Codex smoke scripts local only

--- scripts/run_codex_hook_smoke.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-set -eu
-
-ROOT_DIR="$(cd "$(dirname "$0")/.." && pwd)"
-
-"$ROOT_DIR/scripts/check_codex_setup.sh"
-"$ROOT_DIR/scripts/test_pre_tool_hook.sh"
-
-printf '%s\n' "run_codex_hook_smoke: OK"
